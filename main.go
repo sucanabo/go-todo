da793945bb8fa09fce3bb7eff42a818a809a9e83
felixdinh <felixdinhit@gmail.com>
2024-01-02 15:08:46 +0700

    todo_items update

--- main.go
+++ main.go
@@ -38,6 +38,16 @@ func (TodoItemCreation) TableName() string {
 	return ToDoItemsModel{}.TableName()
 }
 
+type TodoItemUpdate struct {
+	Title       string  `json:"title" gorm:"column:title;"`
+	Description *string `json:"description" gorm:"column:description;"`
+	Status      string  `json:"status" gorm:"column:status;"`
+}
+
+func (TodoItemUpdate) TableName() string {
+	return ToDoItemsModel{}.TableName()
+}
+
 func main() {
 	dsn := os.Getenv("DB_CONN_STR")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -55,7 +65,7 @@ func main() {
 			items.POST("", CreateItem(db))
 			items.GET("")
 			items.GET("/:id", GetItem(db))
-			items.PATCH("/:id")
+			items.PATCH("/:id", UpdateItem(db))
 			items.DELETE("/:id")
 		}
 	}
@@ -115,6 +125,38 @@ func GetItem(db *gorm.DB) func(c *gin.Context) {
 
 		c.JSON(http.StatusOK, gin.H{
 			"data": data,
+		})
+	}
+
+}
+func UpdateItem(db *gorm.DB) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItemUpdate
+
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": true,
 		})
 	}
 
